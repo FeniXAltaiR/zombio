8d9d283c21fd7ef25ad9f848082df87971413a32
Oleg <gameoveran@yandex.ru>
2019-07-20 12:23:30 +0300

    Optimize upgrading weapons

--- src/client/weapons.js
+++ src/client/weapons.js
@@ -4,24 +4,23 @@ const weapons = document.querySelector('.weapons')
 
 export const updateWeaponsBar = me => {
   if (weapons.innerHTML) return
-  
-  if (me.weapon === 'pistol') {
+
+  const {level} = me.experience
+  if (me.weapon === 'pistol' && level >= 2) {
     ['uzi', 'rifle', 'shotgun'].forEach(weapon => {
       const btn = document.createElement('button')
       btn.innerHTML = weapon
       btn.onclick = e => {
-        e.stopPropagation()
         updateWeapon(weapon)
         weapons.innerHTML = ''
       }
       weapons.appendChild(btn)
     })
-  } else if (['uzi', 'rifle', 'shotgun'].includes(me.weapon)) {
+  } else if (['uzi', 'rifle', 'shotgun'].includes(me.weapon) && level >= 3) {
     ['machinegun', 'sniper_rifle', 'auto_shotgun'].forEach(weapon => {
       const btn = document.createElement('button')
       btn.innerHTML = weapon
       btn.onclick = e => {
-        e.stopPropagation()
         updateWeapon(weapon)
         weapons.innerHTML = ''
       }
